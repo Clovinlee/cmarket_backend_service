9aa49501db577b3880b2560ab01194f6a6af94d1
Chrisanto Sinatra <mxzeromxzero6@gmail.com>
2025-04-20 20:55:02 +0700

    feat(controllers): add & implement required controller with created services

---
+++ src/main/java/com/chris/cmarket/Controllers/RarityController.java
@@ -0,0 +1,10 @@
+package com.chris.cmarket.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class RarityController {
+
+}
