496215fd79db5b25fc75f48d52ef04b9c1ae2a30
Chrisanto Sinatra <mxzeromxzero6@gmail.com>
2025-07-27 20:28:04 +0700

    feat(oauth_exception_log): add log on oauth exception try

--- src/main/java/com/chris/cmarket/Auth/Handler/AuthHandler.java
+++ src/main/java/com/chris/cmarket/Auth/Handler/AuthHandler.java
@@ -1,6 +1,11 @@
 package com.chris.cmarket.Auth.Handler;
 
-import lombok.AllArgsConstructor;
+import com.chris.cmarket.Auth.Exception.EmailAlreadyExistException;
+import com.chris.cmarket.Auth.Exception.OAuthException;
+import com.chris.cmarket.Common.Constant.CmarketLoadOrderConstant;
+import com.chris.cmarket.Common.Response.APIResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.annotation.Order;
@@ -10,16 +15,33 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.chris.cmarket.Auth.Exception.EmailAlreadyExistException;
-import com.chris.cmarket.Common.Constant.CmarketLoadOrderConstant;
-import com.chris.cmarket.Common.Response.APIResponse;
-
 @RestControllerAdvice
-@AllArgsConstructor
 @Order(CmarketLoadOrderConstant.DEFAULT_PRIORITY)
+@Slf4j
 public class AuthHandler {
-
     private final MessageSource messageSource;
+
+    private final boolean isDebugMode;
+
+    public AuthHandler(MessageSource messageSource, @Value("${app.debug-mode:false}") boolean isDebugMode) {
+        this.messageSource = messageSource;
+        this.isDebugMode = isDebugMode;
+    }
+
+    @ExceptionHandler(OAuthException.class)
+    public ResponseEntity<APIResponse<String>> handleOAuthException(OAuthException ex) {
+        if (isDebugMode) {
+            log.error("[OAuthException-Debug] URI: {}, Status: {}, Response: {}, Message: {}",
+                    ex.getUri(),
+                    ex.getStatusCode(),
+                    ex.getResponse(),
+                    ex.getMessage(),
+                    ex);
+        }
+
+        APIResponse<String> response = APIResponse.failed("OAuth error: " + ex.getMessage());
+        return new ResponseEntity<>(response, ex.getStatusCode());
+    }
 
     @ExceptionHandler(EmailAlreadyExistException.class)
     public ResponseEntity<APIResponse<String>> handleEmailAlreadyExistException(EmailAlreadyExistException ex) {
