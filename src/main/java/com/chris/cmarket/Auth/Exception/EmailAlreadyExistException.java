de47c34d43098683d282ed2d7d8421e992463569
Chrisanto Sinatra <mxzeromxzero6@gmail.com>
2025-07-27 20:07:15 +0700

    feat(exception_log_handle): added log error on exception handler

--- src/main/java/com/chris/cmarket/Auth/Exception/EmailAlreadyExistException.java
+++ src/main/java/com/chris/cmarket/Auth/Exception/EmailAlreadyExistException.java
@@ -1,15 +1,15 @@
 package com.chris.cmarket.Auth.Exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.Serial;
+
+@Getter
+@AllArgsConstructor
 public class EmailAlreadyExistException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private final String email;
-
-    public EmailAlreadyExistException(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
