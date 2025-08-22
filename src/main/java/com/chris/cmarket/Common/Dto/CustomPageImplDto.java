d854861b49897a416f0c3af1e11b803c5d826723
Chrisanto Sinatra <mxzeromxzero6@gmail.com>
2025-08-22 19:51:33 +0700

    feat(custom_page_impl): modify custom page impl to fetch be buildable from Page interface

--- src/main/java/com/chris/cmarket/Common/Dto/CustomPageImplDto.java
+++ src/main/java/com/chris/cmarket/Common/Dto/CustomPageImplDto.java
@@ -1,16 +1,46 @@
 package com.chris.cmarket.Common.Dto;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @NoArgsConstructor
-public class CustomPageImplDto<T> {
+@AllArgsConstructor
+public class CustomPageImplDto<T> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     protected List<T> content;
-    protected int totalElements;
+    protected long totalElements;
+    private int totalPages;
+    private int pageNumber;
+    private int pageSize;
+
+    /**
+     * Create custom pagination implementation for stable JSON & test
+     *
+     * @param page Pagination result
+     */
+    public CustomPageImplDto(Page<T> page) {
+        this.content = page.getContent();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.pageNumber = page.getNumber();
+        this.pageSize = page.getSize();
+    }
+
+    /**
+     * @return true if empty
+     */
+    public boolean isEmpty() {
+        return this.getTotalElements() == 0;
+    }
 }
