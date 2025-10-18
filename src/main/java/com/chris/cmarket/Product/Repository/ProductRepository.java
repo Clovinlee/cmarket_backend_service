8f46c41ef5b4a723e1a71981fb601b18542e4dea
Clovinlee <mxzeromxzero6@gmail.com>
2025-10-18 14:45:30 +0700

    feat: add send order event to kafka functionality

--- src/main/java/com/chris/cmarket/Product/Repository/ProductRepository.java
+++ src/main/java/com/chris/cmarket/Product/Repository/ProductRepository.java
@@ -2,8 +2,12 @@ package com.chris.cmarket.Product.Repository;
 
 import java.util.Optional;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.chris.cmarket.Product.Model.ProductModel;
@@ -17,4 +21,11 @@ public interface ProductRepository extends JpaRepository<ProductModel, Long>, Jp
     Optional<ProductModel> findFirstByNameContaining(String name);
 
     Optional<ProductModel> findBySlug(String slug);
+
+    // Pesismistic = wait until transaction is over
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM ProductModel p WHERE p.slug = :slug")
+    Optional<ProductModel> findBySlugForUpdate(@Param("slug") String slug);
+
+
 }
